fc6928722a43f20c9ec313d23dffac1eb54f00d0
naoki-1127 <73074569+naoki-1127@users.noreply.github.com>
2022-07-14 00:47:20 +0900

    create env

--- src/main.go
+++ src/main.go
@@ -2,16 +2,37 @@ package main
 
 import (
 	"io"
+	"log"
 	"net/http"
 	"os"
 
 	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
 )
 
 func main() {
-	gin.DisableConsoleColor()
+
 	f, _ := os.Create("gin.log")
+	err := godotenv.Load(".env")
+
+	gin.DisableConsoleColor()
+
 	gin.DefaultWriter = io.MultiWriter(f, os.Stdout)
+
+	DBUSER := os.Getenv("DB_USER")
+	PASSWORD := os.Getenv("PASSWORD")
+	HOST := os.Getenv("HOST")
+	PORTN := os.Getenv("PORTN")
+	DBNAME := os.Getenv("DBNAME")
+
+	dsn := DBUSER + ":" + PASSWORD + "@tcp(" + HOST + ":" + PORTN + ")/" + DBNAME + "?charset=utf8mb4&parseTime=True&loc=Local"
+	log.Printf(dsn)
+	_, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		panic(err.Error())
+	}
 
 	r := gin.Default()
 	r.LoadHTMLGlob("views/*")
